1c60f83678041574d501b108c3846bf5262fc838
Vinicius de Andrade <viniciusdeandrade@gmail.com>
2020-04-16 14:04:17 -0300

    Herança de templates

--- app/__init__.py
+++ app/__init__.py
@@ -6,5 +6,6 @@ from config import Config
 
 # Criando nosso app
 app = Flask(__name__)
+app.config.from_object(Config)
 
 from app import routes
--- app/routes.py
+++ app/routes.py
@@ -7,6 +7,10 @@ from datetime import datetime
 # Criando nossa primeira rota
 @app.route('/')
 def index():
+    return render_template('index.html')  
+
+@app.route('/brasil')
+def brazil():
     # Fazendo a requisição para a API
     request = requests.get('https://api.covid19.finspect.me/brcovid19/map')
 
@@ -16,14 +20,9 @@ def index():
     # Pegando a data atual
     date = datetime.now().strftime(format="%d/%m/%Y")
 
-    return render_template('index.html', states=states_data, date=date)  
-
-
-@app.route('/brasil')
-def brazil():
-    return "<h1>Hello Brasil!</h1>"
+    return render_template('brazil.html', states=states_data, date=date)  
 
 
 @app.route('/mundo')
 def world():
-    return "<h1>Hello Mundo!</h1>"
+    return render_template("world.html", world="World")
